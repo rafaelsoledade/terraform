f807278878a0ac0d9ad293b26a621c2260907298
Rafael Matos <rafaelsoledadem@gmail.com>
2020-05-12 18:27:11 +0100

    Update variables.tf

--- servicebus/variables.tf
+++ servicebus/variables.tf
@@ -1,1 +1,32 @@
+variable "azure_region" {
+  description = "The Azure Region in which all resources in this example should be created."
+  type    = string
+  default = "westus"
+}
 
+variable "resource_env" {
+  description = "The environment prefix which should be used for all resources"
+  type    = string
+  default = "DEV"
+}
+
+variable "resource_group_name" {
+  description = "The Resource Group under which the service bus is created"
+  type    = string
+  #default = "VER_AZURE_DO_BANCO"
+  default="rg"
+}
+
+variable "servicebus_resource_name" {
+  description = "The service bus under which the topics and subscriptions will be created"
+  type    = string
+  #default = "VER_AZURE_DO_BANCO"
+  default="testServiceBus"
+}
+
+variable "sbt_resource_name" {
+  description = "The Topic under which the subscriptions will be created"
+  type    = string
+  #default = "VER_AZURE_DO_BANCO"
+  default="testSbt"
+}
