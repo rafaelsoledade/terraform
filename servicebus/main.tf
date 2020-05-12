5ec5c33c34521091128f4d13c252e18a9f74738c
Rafael Matos <rafaelsoledadem@gmail.com>
2020-05-12 18:22:36 +0100

    Update main.tf

--- servicebus/main.tf
+++ servicebus/main.tf
@@ -3,44 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "${var.resource_env}-resources"
-  location = "${var.azure_region}"
+  name     = "testing-resource-group"
+  location = "westus"
 }
 
-resource "azurerm_servicebus_namespace" "example" {
-  name                = "${var.resource_env}-sbnamespace"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  sku                 = "Standard"
-}
-
-resource "azurerm_servicebus_namespace_authorization_rule" "example" {
-  name                = "${var.resource_env}-sbnauth"
-  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  send                = true
-  listen              = true
-  manage              = true
-}
-
-resource "azurerm_servicebus_topic" "example" {
-  name                = "${var.resource_env}-sbtopic"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-  enable_partitioning = true
-}
-
-resource "azurerm_servicebus_subscription" "example" {
-  name                = "${var.resource_env}-sbsubscription"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-  topic_name          = "${azurerm_servicebus_topic.example.name}"
-  max_delivery_count  = 1
-}
-
-resource "azurerm_servicebus_queue" "example" {
-  name                = "${var.resource_env}-sbqueue"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-  enable_partitioning = true
-}
