4f27bb2a54bba4a295e443d788eadc89289e707e
Rafael Matos <rafaelsoledadem@gmail.com>
2020-05-12 18:12:43 +0100

    Update main.tf
    Testing

--- servicebus/main.tf
+++ servicebus/main.tf
@@ -2,40 +2,45 @@ provider "azurerm" {
   features {}
 }
 
-# Creating a Resource Group
-#resource "azurerm_resource_group" "rg" {
-#        name = "testServiceGroup"
-#        location = "${var.azure_region}"
-#}
-
-
-# Creating a Service Bus
-resource "azurerm_servicebus_namespace" "${var.servicebus_resource_name}" {
-  name                = "${var.servicebus_resource_name}"
-  location            = azurerm_resource_group.${var.resource_group_name}.location
-  resource_group_name = azurerm_resource_group.${var.resource_group_name}.name
-  sku                 = "Standard"
-
-  tags = {
-    source = "terraform"
-  }
+resource "azurerm_resource_group" "example" {
+  name     = "${var.resource_env}-resources"
+  location = "${var.azure_region}"
 }
 
+resource "azurerm_servicebus_namespace" "example" {
+  name                = "${var.resource_env}-sbnamespace"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  sku                 = "Standard"
+}
 
-# Creating Topic (e.g. naming convention DEV-sbt-fpcommission-northeu)
-resource "azurerm_servicebus_topic" "${var.sbt_resource_name}" {
-  name                = "${var.resource_env}-sbt-fpcommission-${var.azure_region}"
-  resource_group_name = azurerm_resource_group.${var.resource_group_name}.name
-  namespace_name      = azurerm_servicebus_namespace.${var.servicebus_resource_name}.name
+resource "azurerm_servicebus_namespace_authorization_rule" "example" {
+  name                = "${var.resource_env}-sbnauth"
+  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  send                = true
+  listen              = true
+  manage              = true
+}
+
+resource "azurerm_servicebus_topic" "example" {
+  name                = "${var.resource_env}-sbtopic"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
   enable_partitioning = true
 }
 
-
-# Creating a subscription for a given topic (e.g. DEV-subs-fpdebitcard-northeu)
-resource "azurerm_servicebus_subscription" "testSubscription" {
-  name                = "${var.resource_env}-subs-fpdebitcard-${var.azure_region}"
-  resource_group_name = azurerm_resource_group.${var.resource_group_name}.name
-  namespace_name      = azurerm_servicebus_namespace.${var.servicebus_resource_name}.name
-  topic_name          = azurerm_servicebus_topic.${var.sbt_resource_name}.name
+resource "azurerm_servicebus_subscription" "example" {
+  name                = "${var.resource_env}-sbsubscription"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
+  topic_name          = "${azurerm_servicebus_topic.example.name}"
   max_delivery_count  = 1
+}
+
+resource "azurerm_servicebus_queue" "example" {
+  name                = "${var.resource_env}-sbqueue"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
+  enable_partitioning = true
 }
