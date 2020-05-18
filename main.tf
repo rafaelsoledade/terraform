ef117c89bf16deda9a116d0ae5c13c8774e80214
Rafael Matos <rafaelsoledadem@gmail.com>
2020-05-18 17:14:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "azurerm" {
 }
 
 # Creating a Resource Group
-resource "azurerm_resource_group" "var.resource_group_name" {
+resource "azurerm_resource_group" "testRG" {
         name = "var.resource_group_name"
         location = "var.azure_region"
 }
 
 
 # Creating a Service Bus
-resource "azurerm_servicebus_namespace" "var.servicebus_resource_name" {
+resource "azurerm_servicebus_namespace" "serviceBus" {
   name                = "var.servicebus_resource_name"
   location            = azurerm_resource_group.var.resource_group_name.location
   resource_group_name = azurerm_resource_group.var.resource_group_name.name
@@ -23,7 +23,7 @@ resource "azurerm_servicebus_namespace" "var.servicebus_resource_name" {
 
 
 # Creating Topic (e.g. naming convention DEV-sbt-fpcommission-northeu)
-resource "azurerm_servicebus_topic" "var.sbt_resource_name" {
+resource "azurerm_servicebus_topic" "testTopic" {
   name                = "${var.resource_env}-sbt-fpcommission-${var.azure_region}"
   resource_group_name = azurerm_resource_group.var.resource_group_name.name
   namespace_name      = azurerm_servicebus_namespace.var.servicebus_resource_name.name
@@ -32,7 +32,7 @@ resource "azurerm_servicebus_topic" "var.sbt_resource_name" {
 
 
 # Creating a subscription for a given topic (e.g. DEV-subs-fpdebitcard-northeu)
-resource "azurerm_servicebus_subscription" "testSubscription" {
+resource "azurerm_servicebus_subscription" "testSubs" {
   name                = "${var.resource_env}-subs-fpdebitcard-${var.azure_region}"
   resource_group_name = azurerm_resource_group.var.resource_group_name.name
   namespace_name      = azurerm_servicebus_namespace.var.servicebus_resource_name.name
