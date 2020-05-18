f1ab3c07bee5a598990d5dc4365e020e5c74cba9
Rafael Matos <rafaelsoledadem@gmail.com>
2020-05-18 17:20:59 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "Namespace Connection String" {
-  value = "azurerm_servicebus_namespace.var.servicebus_resource_name.default_primary_connection_string"
+  value = azurerm_servicebus_namespace.var.servicebus_resource_name.default_primary_connection_string
 }
 
 output "Shared Access Policy PrimaryKey" {
-  value = "azurerm_servicebus_namespace.var.servicebus_resource_name.default_primary_key"
+  value = azurerm_servicebus_namespace.var.servicebus_resource_name.default_primary_key
 }
